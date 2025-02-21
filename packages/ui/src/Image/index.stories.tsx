7409742c2a8ad1d9328deb76cf59cbbb9992cfbb
SuyeonSun <suninisy@gmail.com>
2025-02-21 11:56:50 +0900

    lazy loading 적용

--- packages/ui/src/Image/index.stories.tsx
+++ packages/ui/src/Image/index.stories.tsx
@@ -1,4 +1,4 @@
-import {Image, sizeMap} from '.'
+import {Image, sizeMap, ImageProps} from '.'
 
 const meta = {
     title: 'base/image',
@@ -11,15 +11,26 @@ const meta = {
             },
             description: '이미지 비율',
         },
+        lazy: {
+            control: {type: 'boolean'},
+            description: '지연 로딩 여부',
+        },
     },
 }
 
 export default meta
 
-export const 이미지 = ({size}: any) => {
+export const 이미지 = ({size, lazy}: ImageProps) => {
     return (
         <div>
-            <Image src="https://picsum.photos/200/300" size={size} />
+            <Image src="https://picsum.photos/200/300" size={size} lazy={lazy} />
+            <Image src="https://picsum.photos/200/300" size={size} lazy={lazy} />
+            <Image src="https://picsum.photos/200/300" size={size} lazy={lazy} />
+            <Image src="https://picsum.photos/200/300" size={size} lazy={lazy} />
+            <Image src="https://picsum.photos/200/300" size={size} lazy={lazy} />
+            <Image src="https://picsum.photos/200/300" size={size} lazy={lazy} />
+            <Image src="https://picsum.photos/200/300" size={size} lazy={lazy} />
+            <Image src="https://picsum.photos/200/300" size={size} lazy={lazy} />
         </div>
     )
 }
--- packages/ui/src/Image/index.tsx
+++ packages/ui/src/Image/index.tsx
@@ -1,5 +1,6 @@
 import classNames from 'classnames/bind'
 import styles from './image.module.scss'
+import {useInView} from 'react-intersection-observer'
 
 const cx = classNames.bind(styles)
 
@@ -24,19 +25,46 @@ export interface ImageProps {
     src: string
     alt?: string
     size: keyof typeof sizeMap
+    lazy: boolean
 }
 
-export function Image({src, alt, size}: ImageProps) {
+export function Image({src, alt, size, lazy}: ImageProps) {
+    if (lazy === true) {
+        return <LazyImage src={src} alt={alt} size={size} />
+    } else {
+        return <BaseImage src={src} alt={alt} size={size} />
+    }
+}
+
+interface BaseImageProps {
+    src: string
+    alt?: string
+    size: keyof typeof sizeMap
+}
+
+function BaseImage({src, alt, size}: BaseImageProps) {
+    const isOriginal = size === 'original'
+
     return (
-        <div className={cx({'img-container': true, [`${size}`]: true})}>
-            <img
-                src={src}
-                alt={alt || 'img'}
-                className={cx({
-                    'img-with-ratio': size === 'original',
-                    'img-without-ratio': size !== 'original',
-                })}
-            />
+        <div className={cx({'img-container': true, [`${size}`]: true, original: isOriginal})}>
+            <img src={src} alt={alt || 'img'} />
+        </div>
+    )
+}
+
+interface LazyImageProps {
+    src: string
+    alt?: string
+    size: keyof typeof sizeMap
+}
+
+function LazyImage({src, alt, size}: LazyImageProps) {
+    const {ref, inView} = useInView({triggerOnce: true, threshold: 0})
+    const isOriginal = size === 'original'
+
+    return (
+        <div ref={ref} className={cx({'img-container': true, [`${size}`]: true, original: isOriginal})}>
+            <img src={inView ? src : undefined} alt={alt || 'img'} />
         </div>
     )
 }
